ef9a9e288351bc8dc2fd39c9cd4486fa00f06632
jdyen <jdl.yen@gmail.com>
2019-08-20 07:31:40 +1000

    first commit of scripts

---
+++ R/calc-growth-metrics.R
@@ -0,0 +1,13 @@
+# calculate common metrics of growth to compare with longitudinal measures
+
+# use pre-loaded data
+
+# average growth
+
+# age-at-maturity
+
+
+# annual growth (= functional)
+
+# annual size-at-age
+
---
+++ R/load-data.R
@@ -0,0 +1,19 @@
+# load fGWAS data set
+setwd("~/Dropbox/research/functional-gwas/")
+
+# load data sets
+age_data <- read.csv("data/GP_2018_covariates_wAgeData_updated_wsibgroups_qvalues_inddiv_approx_coords191018.csv",
+                     stringsAsFactors = FALSE)
+snp_data <- read.csv("data/gp_snp_data_012_format_3775loci.txt",
+                     stringsAsFactors = FALSE)
+
+# check columns
+
+
+# match rows of data sets
+
+
+# filter to length, age, SNPs, and covariates
+
+
+# return
